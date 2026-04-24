6eaf88761113e3ab7d5a8b5c14fccb1cf1a1c7f1
Subhasish Das <dassubhasish760@gmail.com>
2026-04-25 00:33:23 +0530

    Updated resume link in portfolio

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,6 @@
 function Resume() {
     return ( 
-        <link rel="stylesheet" href="https://drive.google.com/file/d/1ap2X5W9WpJCwKzVJdf0hgScMsHLUFwsU/view?usp=sharing" />
+        <link rel="stylesheet" href="https://drive.google.com/file/d/11gSdCazAJgHLW7-JfEJ_M2fB-MuOY0Fq/view?usp=sharing" />
     );
 }
 
